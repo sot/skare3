16c86813278427aaad2344d311a9fd2229a09a56
John ZuHone <jzuhone@gmail.com>
2017-07-21 11:40:03 -0400

    Clean up this script and simplify it

--- build_all_packages.sh
+++ build_all_packages.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-CONDA_ROOT="$HOME/Projects/ska/builds"
+
+SKA_CONDA_ROOT="$HOME/Projects/ska"
+export SKA_TOP_SRC_DIR="${SKA_CONDA_ROOT}/src"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "Building $line."
-    conda-build --croot $CONDA_ROOT --old-build-string $line
+    conda-build --croot ${SKA_CONDA_ROOT}/builds $line
 done < "build_order.txt"
