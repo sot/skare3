f7d41fcaf4febcef2b0423d2d9d447cc82866ae1
John ZuHone <jzuhone@gmail.com>
2017-07-24 10:51:18 -0400

    Separate the function of building the package into another script

--- build_all_packages.sh
+++ build_all_packages.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-SKA_CONDA_ROOT="$HOME/Projects/ska"
-export SKA_TOP_SRC_DIR="${SKA_CONDA_ROOT}/src"
-
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "Building $line."
-    conda-build --croot ${SKA_CONDA_ROOT}/builds $line
+    . build_one_package.sh $line
 done < "build_order.txt"
