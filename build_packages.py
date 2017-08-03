11716c764e0f20121bd0bded55c3248f4d26c3a8
John ZuHone <jzuhone@gmail.com>
2017-08-03 10:15:45 -0400

    First crack at a Python script for package building

---
+++ build_packages.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import conda.cli
+import os
+import argparse
+
+build_list = "build_order.txt"
+
+if os.uname().sysname == "Darwin":
+    os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
+
+class SkaBuilder(object):
+
+    def __init__(self, ska_root=None):
+        if ska_root is None:
+            ska_root = "/data/acis/ska3_pkg/"
+        self.ska_root = ska_root
+        self.ska_build_dir = os.path.join(self.ska_root, "builds")
+        os.environ["SKA_TOP_SRC_DIR"] = os.path.join(self.ska_root, "src")
+
+    def build_one_package(self, name):
+
+        print("Building package %s." % name)
+
+        cmd_list = ["conda-build", 
+                    "--croot %s/builds" % self.ska_root,
+                    "--no-anaconda-upload", name]
+
+        conda.cli.main(*cmd_list)
+
+    def build_all_packages(self):
+        with open(build_list, "r") as f:
+            for line in f.readlines():
+                pkg_name = line.strip()
+                if not pkg_name.startswith("#"):
+                    self.build_one_package(pkg_name)
+
+
+    def build_updated_packages(self):
+        pass
+
+parser = argparse.ArgumentParser(description="Build Ska Conda packages.")
+
+parser.add_argument("mode", type=str, help="The build mode. Either 'all' for all "
+                                           "packages, 'updated' for updated "
+                                           "packages, or a specific package name.")
+parser.add_argument("--ska_root", type=str, 
+                    help="The path to the root directory for the Ska packages. "
+                         "Default: /data/acis/ska3_pkg")
+
+args = parser.parse_args()
+
+ska_builder = SkaBuilder(ska_root=args.ska_root)
+
+if args.mode == "all":
+    ska_builder.build_all_packages()
+elif args.mode == "updated":
+    ska_builder.build_updated_packages()
+else:
+    ska_builder.build_one_package(args.mode)
