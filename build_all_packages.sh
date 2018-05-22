8e7a53f90d30dd20307549177ca66a3a01120ad7
John ZuHone <jzuhone@gmail.com>
2018-05-22 10:06:37 -0400

    We don't use these anymore

--- build_all_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    if [[ ${line:0:1} != '#' ]]; then
-        . build_one_package.sh $line
-    fi
-done < "build_order.txt"
--- build_one_package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SKA_CONDA_ROOT="$HOME/Projects/ska"
-export SKA_TOP_SRC_DIR="${SKA_CONDA_ROOT}/src"
-export MACOSX_DEPLOYMENT_TARGET="10.9"
-
-echo "Building $1."
-conda-build --croot ${SKA_CONDA_ROOT}/builds --no-anaconda-upload $1
