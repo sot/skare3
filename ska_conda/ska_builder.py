1dd5d3bb4c001468df36577d889e0b24aca3e525
John ZuHone <jzuhone@gmail.com>
2018-05-22 14:34:15 -0400

    Bugfixes

--- ska_conda/ska_builder.py
+++ ska_conda/ska_builder.py
@@ -18,6 +18,8 @@ class SkaBuilder(object):
         os.environ["SKA_TOP_SRC_DIR"] = self.ska_src_dir
 
     def _clone_repo(self, name):
+        if name == "ska":
+            return
         print("Cloning source %s." % name)
         clone_path = os.path.join(self.ska_src_dir, name)
         if os.path.exists(clone_path):
@@ -44,12 +46,13 @@ class SkaBuilder(object):
                 if not pkg_name.startswith("#"):
                     self._clone_repo(pkg_name)
 
-    def _pull_repo(self, name):
+    def _get_repo(self, name):
+        if name == "ska":
+            return None
         repo_path = os.path.join(self.ska_src_dir, name)
         if not os.path.exists(repo_path):
             self._clone_repo(name)
         repo = git.Repo(repo_path)
-        repo.remote().pull()
         return repo
 
     def _build_package(self, name):
@@ -61,7 +64,9 @@ class SkaBuilder(object):
         subprocess.run(cmd_list)
 
     def build_one_package(self, name):
-        self._pull_repo(name)
+        repo = self._get_repo(name)
+        if repo is not None:
+            repo.remote().pull()
         self._build_package(name)
 
     def build_updated_packages(self, new_only=True):
@@ -69,9 +74,14 @@ class SkaBuilder(object):
             for line in f.readlines():
                 pkg_name = line.strip()
                 if not pkg_name.startswith("#"):
-                    repo = self._pull_repo(pkg_name)
-                    current_tag = repo.tags[-1].name
-                    if repo.tags[-1].name != current_tag or not new_only:
+                    repo = self._get_repo(pkg_name)
+                    if repo is None:
+                        build = True
+                    else:
+                        current_tag = repo.tags[-1].name
+                        repo.remote().pull()
+                        build = repo.tags[-1].name != current_tag
+                    if build or not new_only:
                         self._build_package(pkg_name)
 
     def build_all_packages(self):
