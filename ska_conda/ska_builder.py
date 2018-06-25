eca8683a4b31436c7ce767c48f74a136af601254
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-06-25 17:26:58 -0400

    Fetch recent tags from remote into clone

--- ska_conda/ska_builder.py
+++ ska_conda/ska_builder.py
@@ -42,6 +42,8 @@ class SkaBuilder(object):
                 repo = git.Repo.clone_from(url, clone_path)
         else:
             repo = git.Repo(clone_path)
+            repo.remotes.origin.fetch()
+            repo.remotes.origin.fetch("--tags")
         assert not repo.is_dirty()
         # I think we want the commit/tag with the most recent date, though
         # if we actually want the most recently created tag, that would probably be
