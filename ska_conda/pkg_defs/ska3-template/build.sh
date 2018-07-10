ba534703d45e1ede74dcdf768972dd85de699bc0
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-07-10 17:03:44 -0400

    Change ska_envs permissions +x and to prefix rewriting in the sh files

--- ska_conda/pkg_defs/ska3-template/build.sh
+++ ska_conda/pkg_defs/ska3-template/build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-chmod +x ${RECIPE_DIR}/bin/skare
-chmod +x ${RECIPE_DIR}/bin/flt_envs
-cp -a ${RECIPE_DIR}/bin/skare ${PREFIX}/bin
-cp -a ${RECIPE_DIR}/bin/flt_envs ${PREFIX}/bin
-cp -a ${RECIPE_DIR}/bin/ska_envs.sh ${PREFIX}/bin
-cp -a ${RECIPE_DIR}/bin/ska_envs.csh ${PREFIX}/bin
-sed -i "s|PREFIXPYTHON|${PREFIX}/bin/python|" ${PREFIX}/bin/flt_envs
-
-
-
-
-
-
+for file in flt_envs ska_envs.sh ska_envs.csh
+   do
+    chmod +x ${RECIPE_DIR}/bin/${file}
+    cp -a ${RECIPE_DIR}/bin/${file} ${PREFIX}/bin
+    sed -i "s|%{PREFIX}%|${PREFIX}|" ${PREFIX}/bin/${file}
+   done
