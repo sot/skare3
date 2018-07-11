31c9220fa77fc0ebfcde4b319191a81820c1853a
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-07-10 23:29:41 -0400

    Update ska3-dev with ska3-flight packages (unversioned)

--- ska_conda/pkg_defs/ska3-dev/make_unversioned_meta.py
+++ ska_conda/pkg_defs/ska3-dev/make_unversioned_meta.py
@@ -3,5 +3,4 @@ import json
 result = subprocess.run(['conda', 'list', '--no-pip', '--json'], stdout=subprocess.PIPE)
 pkgs = json.loads(result.stdout)
 for pkg in pkgs:
-    if pkt['channel'] == 'default':
-        print("   - {name}".format(**pkg))
+    print("   - {name}".format(**pkg))
