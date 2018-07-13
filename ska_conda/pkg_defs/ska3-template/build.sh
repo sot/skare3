f19c31fcfbcb19da75996a93d51b7febe52dd7cb
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-07-13 10:10:18 -0400

    Add empty string as option value for sed -i

--- ska_conda/pkg_defs/ska3-template/build.sh
+++ ska_conda/pkg_defs/ska3-template/build.sh
@@ -4,5 +4,5 @@ for file in flt_envs ska_envs.sh ska_envs.csh
    do
     chmod +x ${RECIPE_DIR}/bin/${file}
     cp -a ${RECIPE_DIR}/bin/${file} ${PREFIX}/bin
-    sed -i "s|%{PREFIX}%|${PREFIX}|" ${PREFIX}/bin/${file}
+    sed -i ""  "s|%{PREFIX}%|${PREFIX}|" ${PREFIX}/bin/${file}
    done
