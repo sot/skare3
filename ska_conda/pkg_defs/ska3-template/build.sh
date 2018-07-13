c15afa3a61b93c5b31f4979536b645ab6dd9ae55
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-07-13 11:42:53 -0400

    Move to a sed syntax that works on linux and osx
    It looks like it is easiest to use the '-i'/inplace option with
    the backup file extension provided and then delete the backup.

--- ska_conda/pkg_defs/ska3-template/build.sh
+++ ska_conda/pkg_defs/ska3-template/build.sh
@@ -4,5 +4,10 @@ for file in flt_envs ska_envs.sh ska_envs.csh
    do
     chmod +x ${RECIPE_DIR}/bin/${file}
     cp -a ${RECIPE_DIR}/bin/${file} ${PREFIX}/bin
-    sed -i ""  "s|%{PREFIX}%|${PREFIX}|" ${PREFIX}/bin/${file}
+    # Replace %{PREFIX}% with the conda build prefix.  It looks like it would
+    # be simpler to just make these three files with the standard conda prefix_placeholder
+    # hard-coded in the template files but this sed operation should be robust if the 
+    # prefix_placehold changes in future versions of conda
+    sed -i.bak -e "s|%{PREFIX}%|${PREFIX}|" ${PREFIX}/bin/${file}
+    rm ${PREFIX}/bin/${file}.bak
    done
