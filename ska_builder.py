97b4068e6f65a8044777a3b0e71bf1b674a4c30d
Tom Aldcroft <taldcroft@gmail.com>
2019-11-21 20:22:56 -0500

    Use old build strings (working)

--- ska_builder.py
+++ ska_builder.py
@@ -106,7 +106,7 @@ def build_package(name):
     cmd_list = ["conda", "build", pkg_path, 
                 "--croot", BUILD_DIR, 
                 # "--no-test", 
-                # "--old-build-string",
+                "--old-build-string",
                 "--python", "3.6",
                 "--no-anaconda-upload", 
                 # "--skip-existing", 
