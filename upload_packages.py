7e806d8dc0ebae7e26d436bcd1b8773c15a23990
Tom Aldcroft <taldcroft@gmail.com>
2020-07-10 14:43:24 -0400

    Allow for pushing files to a local repo

--- upload_packages.py
+++ upload_packages.py
@@ -3,6 +3,7 @@
 import argparse
 import getpass
 import os
+import shutil
 import subprocess
 import json
 from pathlib import Path, PurePosixPath
@@ -15,12 +16,11 @@ def get_opt():
 
     parser.add_argument('packages', metavar='package', type=str, nargs='*',
                         help="Package to upload (default=all packages)")
-    parser.add_argument("--host", type=str, default="kady.cfa.harvard.edu",
-                        help="Remote host name (default='kady.cfa.harvard.edu'")
+    parser.add_argument("--host", type=str, default=None,
+                        help="Remote host name (default=None (local repo)")
     parser.add_argument("--user", type=str, default="aca",
                         help="Remote user name (default='aca'")
     parser.add_argument("--repo-dir",
-                        default='/proj/sot/ska/www/ASPECT_ICXC/ska3-conda/shiny',
                         type=str,
                         help="Root dir for conda package repository")
     parser.add_argument("--dry-run",
@@ -65,7 +65,7 @@ def process_package(args, sftp, pkgs_dir, pkg):
     lstat = pkg_file.stat()
     remote_pkg = PurePosixPath(args.repo_dir, platform, filename)
     try:
-        print(f'Checking for {remote_pkg} on remote server ...', end='')
+        print(f'Checking for {remote_pkg} in repository ...', end='')
         rstat = sftp.stat(str(remote_pkg))
     except Exception:
         exists = False
@@ -81,16 +81,33 @@ def process_package(args, sftp, pkgs_dir, pkg):
         print('already exists')
 
 
+class LocalSFTP:
+    """Stub a couple of methods of the paramiko SFTP for local files.
+    """
+    def stat(self, path):
+        return os.stat(path)
+
+    def put(self, src, dest):
+        shutil.copy(src, dest)
+
+
 def main():
     args = get_opt()
-    password = getpass.getpass(f'Password for {args.user}: ')
 
-    with paramiko.SSHClient() as ssh_client:
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname=args.host, username=args.user, password=password)
+    if args.host:
+        password = getpass.getpass(f'Password for {args.user}: ')
 
-        with ssh_client.open_sftp() as sftp:
-            process_packages(args, sftp)
+        with paramiko.SSHClient() as ssh_client:
+            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh_client.connect(hostname=args.host, username=args.user, password=password)
+
+            with ssh_client.open_sftp() as sftp:
+                process_packages(args, sftp)
+    else:
+        local_sftp = LocalSFTP()
+        for subdir in ('noarch', 'win-64', 'linux-64', 'osx-64'):
+            Path(args.repo_dir, subdir).mkdir(parents=True, exist_ok=True)
+        process_packages(args, local_sftp)
 
 
 if __name__ == '__main__':
