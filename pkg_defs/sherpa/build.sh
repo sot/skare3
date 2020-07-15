db8cd5c20d74eea61a5587893b392b058fb208a6
Tom Aldcroft <taldcroft@gmail.com>
2020-07-15 13:55:28 -0400

    Add recipe for sherpa

---
+++ pkg_defs/sherpa/build.sh
@@ -0,0 +1,12 @@
+
+sed -i.orig "s|#install_dir=build|install_dir=$PREFIX|" setup.cfg
+git update-index --assume-unchanged setup.cfg
+
+export PYTHON_LDFLAGS=" "
+
+$PYTHON setup.py build
+$PYTHON setup.py install
+
+# This headers are known to collide with astropy's extensions and would prevent astropy from building
+rm -f $PREFIX/include/wcs*.h
+
